ff2b80d2a792f06e0aeec6d315c52324441e5658
WolframRhodium <WolframRhodium@users.noreply.github.com>
2022-07-23 17:01:59 +0800

    cuda_source/kernel.hpp: fix frequency location calculation

--- cuda_source/kernel.hpp
+++ cuda_source/kernel.hpp
@@ -11,11 +11,13 @@ extern void filter(float2 * value, int x, int y, int z, int id);
 
 extern "C"
 __global__
-void frequency_filtering(float2 * data, int n, int block_size) {
-    int sq_block_size = block_size * block_size;
+void frequency_filtering(float2 * data, int n, int block_size_1d) {
+    int block_size_x = block_size_1d / 2 + 1;
+    int block_size_y = block_size_1d;
+    int block_size_2d = block_size_y * block_size_x;
 
     for (int x = blockIdx.x * blockDim.x + threadIdx.x; x < n; x += blockDim.x * gridDim.x) {
-        filter(&data[x], x % block_size, x % sq_block_size / block_size, 0, x / sq_block_size);
+        filter(&data[x], x % block_size_x, (x % block_size_2d) / block_size_x, 0, x / block_size_2d);
     }
 }
 )""";
