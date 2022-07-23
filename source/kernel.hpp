62789b3e690c0e14bf93b10ae8b3bc733ec0783c
WolframRhodium <WolframRhodium@users.noreply.github.com>
2022-07-23 09:47:44 +0800

    source/kernel.hpp: fix filter prototype

--- source/kernel.hpp
+++ source/kernel.hpp
@@ -7,7 +7,7 @@ static const auto kernel_header_template = R"""(
 #endif // __CUDACC_RTC__
 
 __device__
-extern void filter(float2 * value, int x, int y, int id);
+extern void filter(float2 * value, int x, int y, int z, int id);
 
 extern "C"
 __global__
@@ -15,7 +15,7 @@ void frequency_filtering(float2 * data, int n, int block_size) {
     int sq_block_size = block_size * block_size;
 
     for (int x = blockIdx.x * blockDim.x + threadIdx.x; x < n; x += blockDim.x * gridDim.x) {
-        filter(&data[x], x % block_size, x % sq_block_size / block_size, x / sq_block_size);
+        filter(&data[x], x % block_size, x % sq_block_size / block_size, 0, x / sq_block_size);
     }
 }
 )""";
